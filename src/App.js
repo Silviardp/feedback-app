a94b6997de1bc760b6c67be7744e0a034aaa913a
Silvia Rocha <silviadprocha@gmail.com>
2021-12-23 23:36:52 +0100

    dynamic values and lists in jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 function App() {
+ const title = 'Blog post'
+ const body = 'This is my blog post'
+ const comments = [
+  {id:1, text: 'comment one'},
+  {id:2, text: 'comment two'},
+  {id:3, text: 'comment three'},
+
+ ]
+
  return (
   <div className="container">
-   <h1>My App</h1>
-   <p>Hello</p>
+   <h1>{title.toUpperCase()}</h1>
+   <p>{body}</p>
+
+   <div className="comments">
+   <h3>Comments ({comments.length})</h3>
+   <ul>
+    {comments.map((comment, index) => (
+     <li key={index}>{comment.text}</li>
+    ))}
+   </ul>
+
+   </div>
   </div>
  )
 }
