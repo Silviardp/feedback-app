8cbf0d25975f49fe08ac81433843062c42d8ab28
Silvia Rocha <silviadprocha@gmail.com>
2022-01-04 21:44:34 +0100

    creation of Header component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
+import Header from "./components/Header"
+
 function App() {
- const title = 'Blog post'
- const body = 'This is my blog post'
- const comments = [
-  {id:1, text: 'comment one'},
-  {id:2, text: 'comment two'},
-  {id:3, text: 'comment three'},
- ]
-
- const loading = false
- const showComments = true
-
  return (
+  <>
+  <Header text={true}/>
   <div className="container">
-   <h1>{title.toUpperCase()}</h1>
-   <p>{body}</p>
-
-   {showComments && (<div className="comments">
-   <h3>Comments ({comments.length})</h3>
-   <ul>
-    {comments.map((comment, index) => (
-     <li key={index}>{comment.text}</li>
-    ))}
-   </ul>
-   </div>)}
-
+   <h1>My app</h1>
   </div>
+  </>
  )
 }
 
