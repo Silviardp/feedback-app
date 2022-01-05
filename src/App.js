c690dacf8adfe8740f50206ffeacf353ec410314
Silvia Rocha <silviadprocha@gmail.com>
2022-01-05 20:42:12 +0100

    state and use state hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import FeedbackItem from "./components/FeedbackItem"
 import Header from "./components/Header"
+
 
 function App() {
  return (
   <>
   <Header />
   <div className="container">
-   <h1>My app</h1>
+   <FeedbackItem />
   </div>
   </>
  )
