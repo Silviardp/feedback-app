7475f9a2c50b2e4960cc1cb8f4a2d1885546e27c
Silvia Rocha <silviadprocha@gmail.com>
2022-01-07 16:20:38 +0100

    added delete function

--- src/App.js
+++ src/App.js
@@ -7,11 +7,18 @@ function App() {
 
  const [feedback, setFeedback] = useState(FeedbackData)
 
+ const deleteFeedback = (id) => {
+  if(window.confirm('Are you sure you want to delete?')) {
+   setFeedback(feedback.filter((item) => item.id !== id))
+  }
+  
+ }
+
  return (
   <>
   <Header />
   <div className="container">
-   <FeedbackList feedback={feedback}/>
+   <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
   </div>
   </>
  )
