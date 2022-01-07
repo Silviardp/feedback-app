8e66f1d6cdba685bf1dc580eaef3f4799aa4a01c
Silvia Rocha <silviadprocha@gmail.com>
2022-01-07 15:57:45 +0100

    installed react icons

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,3 +1,4 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from "./shared/Card"
 
@@ -7,6 +8,9 @@ function FeedbackItem({item}) {
  return (
   <Card>
    <div className="num-display">{item.rating}</div>
+   <button className="close">
+     <FaTimes color="purple" />
+   </button>
    <div className="text-display">{item.text}</div>
   </Card>
  )
