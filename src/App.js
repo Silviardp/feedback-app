eb15638c169bd65ff53d35011bc5d0310f748e5b
Silvia Rocha <silviadprocha@gmail.com>
2022-01-08 23:23:24 +0100

    added about icon link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
 
 function App() {
 
@@ -40,6 +41,7 @@ function App() {
 
      <Route path='/about' element={<AboutPage />}/>
     </Routes> 
+    <AboutIconLink />
    </div>
   </Router>
  )
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,6 @@
 import Card from "../components/shared/Card"
+import {Link} from 'react-router-dom'
+ 
 
 function AboutPage() {
  return (
@@ -8,7 +10,7 @@ function AboutPage() {
     <p>This is a react app to leave feedback for product or service</p>
     <p>Version: 1.0.0</p>
    </div>
-   <a href="/">Back to Home</a>
+   <Link to="/">Back to Home</Link>
   </Card>
  )
 }
