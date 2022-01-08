2ddba5b846de870bbbdd709fcf66e2491901cb7d
Silvia Rocha <silviadprocha@gmail.com>
2022-01-08 19:20:38 +0100

    installed uuid + added new feedback

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,7 @@ import Card from './shared/Card'
 import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
 
  const [text, setText] = useState('')
  const [rating, setRating] = useState(10)
@@ -26,9 +26,23 @@ function FeedbackForm() {
    setText(e.target.value)
  }
 
+ const handleSubmit = (e) => {
+   e.preventDefault()
+   if(text.trim().length > 10) {
+     const newFeedback = {
+       text: text,
+       rating: rating
+     }
+
+     handleAdd(newFeedback)
+
+     setText('')
+   }
+ }
+
  return (
   <Card>
-  <form>
+  <form onSubmit={handleSubmit}>
    <h2>How would you rate your service with us?</h2>
    <RatingSelect  select={(rating) => setRating(rating)}/>
    <div className="input-group">
