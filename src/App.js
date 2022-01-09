a7c7f031cd277553eafb1da720f5d847f85a9da3
Silvia Rocha <silviadprocha@gmail.com>
2022-01-09 17:29:02 +0100

    moved addfeedback function to context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import { v4 as uuidv4 } from 'uuid'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import { useState } from 'react'
 import { FeedbackProvider } from './context/FeedbackContext'
 import FeedbackList from "./components/FeedbackList"
 import Header from "./components/Header"
-import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink'
 
 function App() {
-
- const [feedback, setFeedback] = useState(FeedbackData)
-
- const addFeedback = (newFeedback) => {
-  newFeedback.id = uuidv4()
-  setFeedback([newFeedback, ...feedback])
- }
-
- 
 
  return (
   <FeedbackProvider>
@@ -29,7 +17,7 @@ function App() {
    <Routes>
     <Route exact path="/" element={
      <>
-     <FeedbackForm handleAdd={addFeedback}/>
+     <FeedbackForm />
      <FeedbackStats />
      <FeedbackList />
      </>
--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 import { createContext, useState } from "react"
 
 const FeedbackContext = createContext()
@@ -11,6 +12,11 @@ export const FeedbackProvider = ({ children }) => {
   }
  ])
 
+  const addFeedback = (newFeedback) => {
+  newFeedback.id = uuidv4()
+  setFeedback([newFeedback, ...feedback])
+ }
+
   const deleteFeedback = (id) => {
   if(window.confirm('Are you sure you want to delete?')) {
    setFeedback(feedback.filter((item) => item.id !== id))
@@ -20,6 +26,7 @@ export const FeedbackProvider = ({ children }) => {
  return (<FeedbackContext.Provider value={{
   feedback,
   deleteFeedback,
+  addFeedback,
  }}>
   {children}
  </FeedbackContext.Provider>
