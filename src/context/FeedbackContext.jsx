b857ebfde80b5c47ee2192d45ef98f0d0342040b
Silvia Rocha <silviadprocha@gmail.com>
2022-01-09 17:37:06 +0100

    added edit feedback event

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -7,26 +7,52 @@ export const FeedbackProvider = ({ children }) => {
  const [feedback, setFeedback] = useState([
   {
    id: 1, 
-   text: 'This item is from Context',
-   rating: 10
+   text: 'This is feedback item 1',
+   rating: 10,
+  },
+  {
+   id: 2, 
+   text: 'This is feedback item 2',
+   rating: 9,
+  },
+  {
+   id: 3, 
+   text: 'This is feedback item 3',
+   rating: 7,
   }
  ])
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+   item: {},
+   edit: false,
+  })
+
+  // Add feedback
   const addFeedback = (newFeedback) => {
   newFeedback.id = uuidv4()
   setFeedback([newFeedback, ...feedback])
  }
 
+  // Delete feedback
   const deleteFeedback = (id) => {
   if(window.confirm('Are you sure you want to delete?')) {
    setFeedback(feedback.filter((item) => item.id !== id))
   }
  }
 
+ // Set item to be updated
+ const editFeedback = (item) => {
+  setFeedbackEdit({
+   item,
+   edit: true
+  })
+ }
+
  return (<FeedbackContext.Provider value={{
   feedback,
   deleteFeedback,
   addFeedback,
+  editFeedback
  }}>
   {children}
  </FeedbackContext.Provider>
