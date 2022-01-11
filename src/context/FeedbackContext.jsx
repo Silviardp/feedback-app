7974c45d84aa2c9ebe2303f73f7adcc94669b02f
Silvia Rocha <silviadprocha@gmail.com>
2022-01-11 23:32:48 +0100

    update and delete from json server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -40,15 +40,26 @@ export const FeedbackProvider = ({ children }) => {
  }
 
   // Delete feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
   if(window.confirm('Are you sure you want to delete?')) {
+   await fetch(`/feedback/${id}`, {method: 'DELETE'})
    setFeedback(feedback.filter((item) => item.id !== id))
   }
  }
 
   // Update feedback item
- const updateFeedback = (id, updItem) => {
-  setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item))
+ const updateFeedback = async (id, updItem) => {
+  const response  = await fetch(`/feedback/${id}`, {
+   method: 'PUT', 
+   headers: {
+    'Content-Type': 'application/json', 
+   }, 
+   body: JSON.stringify(updItem)
+ })
+
+  const data = await response.json()
+
+  setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item))
  }
 
  // Set item to be updated
